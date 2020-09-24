383cddf8f1a76bb94fb0f93fdfb205d8970e4619
JuanDuran85 <duran85juan@gmail.com>
2020-09-24 09:02:31 -0300

    agregando consulta a API desde Archivo JSON

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-
     <router-view/>
   </div>
 </template>
@@ -10,12 +9,18 @@
 </style>
 
 <script>
+import axios from 'axios';
 
 export default {
   name: 'App',
   mounted() {
     this.$store.dispatch('infoDashboard');
     this.$store.dispatch('infoMonitor');
+    //buscando la API de manera local con el archivo JSON
+    axios.get("api/dashboard.json").then(response=>{
+      console.log("Info local API");
+      console.log(response.data)
+    })
   },
 }
 </script>
